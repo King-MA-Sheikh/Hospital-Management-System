1dadbf2520d8977cdd6f10ac1054b2f1719c914d
king-MA_Sheikh <161627033+King-MA-Sheikh@users.noreply.github.com>
2025-03-10 15:06:05 +0530

    Hospital Management System
    Hospital Management System GUI in JAVA

---
+++ HospitalManagementSystemGUI/src/hspital/management/system/conn.java
@@ -0,0 +1,24 @@
+package hspital.management.system;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class conn {
+
+    Connection connection;
+    Statement statement;
+
+
+    public conn() {
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/hospital_management_system", "root", "");
+            statement = connection.createStatement();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
